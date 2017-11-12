b4af8df03f22458e95eb558591b61f28e4ebacd0
Joe Morris <joe.morris.dev@gmail.com>
2017-11-12 19:56:32 +0100

    added connection helper method

--- tests/DatabaseTests.java
+++ tests/DatabaseTests.java
@@ -5,5 +5,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import utils.SQLQueries;
+
 public class DatabaseTests {
+
+    //helper method
+    public Connection startConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:mem:test");
+        SQLQueries.createDatabase(conn);
+        return conn;
+    }
+
 }
